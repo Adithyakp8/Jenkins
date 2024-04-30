50cf58ef062139eb84eca41cc3fce21f8c92d43d
Adithyakp8 <adityakp807@gmail.com>
2024-04-30 23:20:01 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        printData();
+    }
+
+    public static void printData() {
+        System.out.println("Hello World");
+    }
+}
